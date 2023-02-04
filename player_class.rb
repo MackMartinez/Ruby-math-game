5dce18ece3d57731841379406e9a14147f6d9283
MackyClap <martimac2320@gmail.com>
2023-02-04 12:37:30 -0500

    code and test player class for correct output using puts

---
+++ player_class.rb
@@ -0,0 +1,30 @@
+class Player
+  attr_accessor :name, :player_lives
+  
+  def initialize(name, lives)
+    @name = name
+    @player_lives = lives
+  end
+
+  def wrong_answer_statement
+    puts "#{@name}: Seriously? No!"
+  end
+
+  def right_answer_statement
+    puts "#{@name}: YES! You are correct."
+  end
+
+end
+
+macky = Player.new("Macky", 3)
+
+=begin
+puts "get name"
+puts macky.name
+
+puts "get lives"
+puts macky.player_lives
+
+puts macky.wrong_answer_statement
+puts macky.right_answer_statement
+=end
