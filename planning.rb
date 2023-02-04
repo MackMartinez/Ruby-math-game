5183b7e02b76abbab66739df95188058a3f8f18f
MackyClap <martimac2320@gmail.com>
2023-02-04 12:36:34 -0500

    wrote out planning for coding the game

---
+++ planning.rb
@@ -0,0 +1,39 @@
+=begin
+
+Task 1: Extract Nouns for Classes
+
+- Players
+- question
+
+Task 2: Write their roles
+
+Question Class
+
+- puts randomly generated math question (sum of 2 numbers, each being a number from 1-20) with current player at beginning of string
+- stores user input
+
+Game loop
+
+- stores who is the current player
+- Method to start game
+- methods to reduce amount of lives if player input != to answer of math question, or don't change if answer is correct
+- Method to print balance of each players lives
+- Method to print game over & Goodbye if any players lives = 0
+
+Initialize
+
+@game_name
+
+Players class
+
+- store the current of lives per player, int
+- method to print response if user answered correctly 
+- method to print response if user didn't answer correctly
+
+Initialize
+
+@player, @player_lives
+
+
+
+=end
