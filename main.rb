49e074b90cb8fbbc618af45d6656a3cbf7bc13dc
MackyClap <martimac2320@gmail.com>
2023-02-04 16:47:41 -0500

    ensure that show_lives_balance method is returning correctly

--- main.rb
+++ main.rb
@@ -2,8 +2,10 @@ require "./game_class.rb"
 require "./player_class.rb"
 require "./question_class.rb"
 
-p1 = Player.new("P1")
-p2 = Player.new("P2")
+p1 = Player.new("P1", 3)
+p2 = Player.new("P2", 3)
 mathGame = Gameloop.new()
 
+
 mathGame.start_game
+mathGame.show_lives_balance(p1, p2)
